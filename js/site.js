09066c7070fe073d8aef18bf055e7ee2ac04949d
drugOnFly <74011651+drugOnFly@users.noreply.github.com>
2021-08-15 19:46:29 +0200

    implement basic calculation logic,
    probably will need to refactor it in the future during results testing

--- js/site.js
+++ js/site.js
@@ -1,4 +1,25 @@
 //Call HelloWorld
 function HelloWorld() {
-    alert('Hello World');
+  alert("Hello World");
+}
+
+function calculateTotalMonthlyPayment(loanAmount, rate, numberOfMonths) {
+  // total monthly payment = (amount loaned) * (rate / 1200) / (1 - (1 + rate / 1200)^(- Number of Months))
+  return (
+    (loanAmount * (rate / 1200)) / (1 - (1 + rate / 1200) ** (-numberOfMonths))
+  );
+}
+
+function calculateRemainingBalance(previousRemainingBalance, principalPayments) {
+  // before the very first month equals the amount of the loan
+
+  return previousRemainingBalance - principalPayments;
+}
+
+function calculateInterestPayment(previousRemainingBalance, rate) {
+  return (previousRemainingBalance * rate) / 1200;
+}
+
+function calculatePrincipalPayment(totalMonthlyPayment, interestPayment) {
+    return totalMonthlyPayment - interestPayment;
 }
